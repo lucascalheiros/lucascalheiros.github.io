ca523c95631bcdc7b2e8060c5f539e0e90b0f76b
Lucas Calheiros <lucascalheirosjllc@gmail.com>
2025-12-18 22:11:47 -0300

    Use repo path with nojekyll

--- ImageComparator/_next/static/eKHwAbfFZE1SfwiBP_7-_/_buildManifest.js
+++ ImageComparator/_next/static/eKHwAbfFZE1SfwiBP_7-_/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ImageComparator//_next/:path+",
+        "destination": "/ImageComparator/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
